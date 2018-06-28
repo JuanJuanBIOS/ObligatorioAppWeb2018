8e10f6cd8c0bc6c709bdeabe5ded807445f5600b
juan-1603 <juanlopezmelsion@gmail.com>
2018-06-27 22:19:27 -0300

    Propiedades casi completas

--- ObligatorioAppWeb/EntidadesCompartidas/Internacionales.cs
+++ ObligatorioAppWeb/EntidadesCompartidas/Internacionales.cs
@@ -10,5 +10,29 @@ namespace EntidadesCompartidas
         //Atributos
         private bool _servicio;
         private string _documentacion;
+
+        //Propiedades
+        public bool Servicio
+        {
+            get {return _servicio;}
+
+            set {_servicio = value;}
+        }
+
+        public string Documentacion
+        {
+            get {return _documentacion;}
+
+            set {_documentacion = value;}
+        }
+
+
+       //Constructor
+        public Internacionales(int pNumero, Companias pCompania, Terminales pTerminal, DateTime pFecha_partida, DateTime pFecha_arribo, int pAsientos, Empleados pEmpleado, bool pServicio, string pDoocumentacion)
+            : base(pNumero, pCompania, pTerminal, pFecha_partida, pFecha_arribo, pAsientos, pEmpleado)
+        {
+            Servicio = pServicio;
+            Documentacion = pDoocumentacion;
+        }
     }
 }
--- ObligatorioAppWeb/EntidadesCompartidas/Viajes.cs
+++ ObligatorioAppWeb/EntidadesCompartidas/Viajes.cs
@@ -15,5 +15,111 @@ namespace EntidadesCompartidas
         private DateTime _fecha_arribo;
         private int _asientos;
         private Empleados _empleado;
+
+
+        //Propiedades
+        public int Numero
+        {
+            get {return _numero;}
+
+            set
+            {       
+                if (value > 0)
+                    _numero = value;
+                else
+                    throw new Exception("El número de viaje no es válido");
+            }
+        }
+
+        public Companias Compania
+        {
+            get {return _compania;}
+
+            set
+            {
+                if (value != null)
+                {
+                    _compania = value;
+                }
+                else
+                {
+                    throw new Exception("La compañía ingresada no es válida");
+                }
+            }
+        }
+
+        public Terminales Terminal
+        {
+            get {return _destino;}
+
+            set
+            {
+                if (value != null)
+                {
+                    _destino = value;
+                }
+                else
+                {
+                    throw new Exception("La terminal ingresada no es válida");
+                }
+            }
+        }
+
+        public DateTime Fecha_partida
+        {
+            get {return _fecha_partida;} //FALTA VERIFICAR QUE SEA POSTERIOR AL DÍA DE HOY Y ADEMÁS VER SI SE PUEDE VERIFICAR QUE SEA ANTERIOR A LA DE ARRIBO
+
+            set {_fecha_partida = value;}
+        }
+
+        public DateTime Fecha_arribo
+        {
+            get { return _fecha_arribo; } //FALTA VERIFICAR QUE SEA POSTERIOR AL DÍA DE HOY Y ADEMÁS VER SI SE PUEDE VERIFICAR QUE SEA POSTERIOR A LA DE PARTIDA
+
+            set {_fecha_arribo = value;}
+        }
+
+        public int Asientos
+        {
+            get {return _asientos;}
+
+            set
+            {       
+                if (value > 0)
+                    _asientos = value;
+                else
+                    throw new Exception("El número de asientos no es válido");
+            }
+        }
+
+        public Empleados Empleado
+        {
+            get {return _empleado;}
+
+            set
+            {
+                if (value != null)
+                {
+                    _empleado = value;
+                }
+                else
+                {
+                    throw new Exception("El empleado ingresado no es válido");
+                }
+            }
+        }
+
+
+       //Constructor
+        public Viajes(int pNumero, Companias pCompania, Terminales pTerminal, DateTime pFecha_partida, DateTime pFecha_arribo, int pAsientos, Empleados pEmpleado)
+        {
+            Numero = pNumero;
+            Compania = pCompania;
+            Terminal = pTerminal;
+            Fecha_partida = pFecha_partida;
+            Fecha_arribo = pFecha_arribo;
+            Asientos = pAsientos;
+            Empleado = pEmpleado;
+        }
     }
 }
