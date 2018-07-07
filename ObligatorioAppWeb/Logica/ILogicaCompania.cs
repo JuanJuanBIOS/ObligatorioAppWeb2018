c768befcacd7d913f97d95ebbcb7221e1b7f7ae5
juan-1603 <juanlopezmelsion@gmail.com>
2018-07-07 19:07:21 -0300

    alta compania

--- ObligatorioAppWeb/Logica/ILogicaCompania.cs
+++ ObligatorioAppWeb/Logica/ILogicaCompania.cs
@@ -9,7 +9,7 @@ namespace Logica
     public interface ILogicaCompania
     {
         Companias Buscar_Compania(string pNombre);
-        void Crear_Compania(Companias C);
+        void Alta_Compania(Companias C);
         void Eliminar_Compania(Companias C);
         void Modificar_Compania(Companias C);
     }
