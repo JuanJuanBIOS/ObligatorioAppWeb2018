16c5cea577f914a9c90df0d5549fd64818a6550f
juapem <juapem@gmail.com>
2018-07-28 20:55:18 +0100

    Master Page arreglado
    Puse el Menu, y saque los links

--- WebSite/ConsultaIndividualViaje.aspx
+++ WebSite/ConsultaIndividualViaje.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="ConsultaIndividualViaje.aspx.cs" Inherits="ConsultaIndividualViaje" %>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
@@ -9,9 +9,6 @@
 <body>
     <form id="form1" runat="server">
     <div>
-    
-        <asp:Repeater ID="RepeaterViajes" runat="server">
-        </asp:Repeater>
     
     </div>
     </form>
---
+++ WebSite/Logueo.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Logica;
+using Logica.Interfaces;
+using EntidadesCompartidas;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        TBCedula.Focus();
+    }
+
+    protected void BtnLogin_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            string _cedula = TBCedula.Text;
+            string _pass = TBContrasenia.Text;
+
+            ILogicaEmpleado FEmpleado = FabricaLogica.getLogicaEmpleado();
+
+            Empleados unEmp = FEmpleado.Login(_cedula, _pass);
+
+            if (unEmp != null)
+            {
+                Session["Empleado"] = unEmp;
+
+                Response.Redirect("BienvenidaEmpleado.aspx", false);
+            }
+            else
+            {
+                LblException.Text = "";
+                LblError.Text = "Cédula y/o contraseña incorrectas.";
+            }
+        }
+
+        catch
+        {
+            LblError.Text = "";
+            LblException.Text = "Error en la base de datos. Contacte con el administrador.";
+        }
+    }
+}
